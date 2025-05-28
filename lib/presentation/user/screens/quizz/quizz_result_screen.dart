b6b498078d1080ec803d4f9518c12f5001158e17
MgSuper <super.dev1001@gmail.com>
2025-05-29 02:41:47 +0700

    Add icon web

--- lib/presentation/user/screens/quizz/quizz_result_screen.dart
+++ lib/presentation/user/screens/quizz/quizz_result_screen.dart
@@ -22,63 +22,65 @@ class QuizResultScreen extends ConsumerWidget {
     final totalQuestions = quiz.questions.length;
     final score = ((correctAnswers / totalQuestions) * 100).round();
 
+    final double horizontalPadding =
+        MediaQuery.of(context).size.width >= 640 ? 48 : 16;
+
     return Scaffold(
       backgroundColor: Colors.transparent,
       appBar: const UserAppBar(title: 'Quiz Result'),
-      body: ResponsiveWrapper(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Quiz Name:',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium!
-                    .copyWith(color: Color(0xFFD8E2E4)),
-              ),
-              Text(
-                quiz.title,
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(color: Color(0xFFD8E2E4)),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Score:',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium!
-                    .copyWith(color: Color(0xFFD8E2E4)),
-              ),
-              Text(
-                '$score%',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(color: Color(0xFFD8E2E4)),
-              ),
-              const SizedBox(height: 24),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  backgroundColor: Color(0xFFD8E2E4),
-                  foregroundColor: Color(0xFF316E79),
+      body: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Quiz Name:',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Color(0xFFD8E2E4)),
+            ),
+            Text(
+              quiz.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(color: Color(0xFFD8E2E4)),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Score:',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Color(0xFFD8E2E4)),
+            ),
+            Text(
+              '$score%',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(color: Color(0xFFD8E2E4)),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                onPressed: () {
-                  ref.invalidate(userQuizScoreProvider); // 👈 Refresh score map
-                  ref.invalidate(
-                      completedQuizIdsProvider); // 👈 Optional, if needed
-                  context.go('/user/quizzes');
-                },
-                child: const Text('Back to Quizzes'),
+                backgroundColor: Color(0xFFD8E2E4),
+                foregroundColor: Color(0xFF316E79),
               ),
-            ],
-          ),
+              onPressed: () {
+                ref.invalidate(userQuizScoreProvider); // 👈 Refresh score map
+                ref.invalidate(
+                    completedQuizIdsProvider); // 👈 Optional, if needed
+                context.go('/user/quizzes');
+              },
+              child: const Text('Back to Quizzes'),
+            ),
+          ],
         ),
       ),
     );

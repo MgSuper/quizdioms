4d6d966c7ac4f0025f10bee23cd8360dab4f3eab
MgSuper <MgSuper@users.noreply.github.com>
2025-06-03 21:03:42 +0000

    deploy: aa6f7afb94684c03bb6b56a98d47800d3c4f6c58

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2009497285"
+    serviceWorkerVersion: "2754181397"
   }
 });
